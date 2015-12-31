daab439ba23ee03b6e0e4a45a6c1be3a1fb5163d
helpfulrobot <chris@silverstripe.com>
2016-01-01 03:25:31 +1300

    Converted to PSR-2

--- code/Linkable.php
+++ code/Linkable.php
@@ -4,26 +4,26 @@
  * have a public Link() function in order to be linkable and a LinkTitle()
  * function to give a relevant Title within the HTML Editor Field
  */
-interface Linkable {
+interface Linkable
+{
 
-	/**
-	 * The Link() function should return a URL which will navigate to the correct
-	 * location to view the Object. A view function should be set up on the Controller
-	 * which will be handling the display of this Object. For information on
-	 * creating such functionality, see:
-	 * http://www.ssbits.com/tutorials/2010/dataobjects-as-pages-part-1-keeping-it-simple/
-	 *
-	 * @return URL string
-	 */
-	public function Link();
-	
-	/**
-	 * The LinkTitle() function should return the Title string that will be used
-	 * within the link view. This should ideally be based on a db field to easily
-	 * recognise the right Object to link to - however it is up to the Developer.
-	 *
-	 * @return Title string
-	 */
-	public function LinkTitle();
-
+    /**
+     * The Link() function should return a URL which will navigate to the correct
+     * location to view the Object. A view function should be set up on the Controller
+     * which will be handling the display of this Object. For information on
+     * creating such functionality, see:
+     * http://www.ssbits.com/tutorials/2010/dataobjects-as-pages-part-1-keeping-it-simple/
+     *
+     * @return URL string
+     */
+    public function Link();
+    
+    /**
+     * The LinkTitle() function should return the Title string that will be used
+     * within the link view. This should ideally be based on a db field to easily
+     * recognise the right Object to link to - however it is up to the Developer.
+     *
+     * @return Title string
+     */
+    public function LinkTitle();
 }
--- tests/DataObjectPickerTest.php
+++ tests/DataObjectPickerTest.php
@@ -1,85 +1,92 @@
 <?php
 
-class DataObjectPickerTest extends FunctionalTest {
+class DataObjectPickerTest extends FunctionalTest
+{
 
-	static $fixture_file = 'dataobjectpicker/tests/DataObjectPickerTest.yml';
+    public static $fixture_file = 'dataobjectpicker/tests/DataObjectPickerTest.yml';
 
-	protected $extraDataObjects = array(
-		'DogOwner',
-		'Dog',
-	);
+    protected $extraDataObjects = array(
+        'DogOwner',
+        'Dog',
+    );
 
-	public function testFieldWithMagicAndValue() {
-		$dog = $this->objFromFixture('Dog', 'Bello');
-		$dogcontroller = new DogController();
-		$dogform = $dogcontroller->getEditForm();
-		$dogform->loadDataFrom($dog);
-		$field = $dogform->Fields()->fieldByName('OwnerID');
-		$this->assertContains('Wolfgang', $field->FieldHolder(), 'Parent DataObject found.');
-	}
+    public function testFieldWithMagicAndValue()
+    {
+        $dog = $this->objFromFixture('Dog', 'Bello');
+        $dogcontroller = new DogController();
+        $dogform = $dogcontroller->getEditForm();
+        $dogform->loadDataFrom($dog);
+        $field = $dogform->Fields()->fieldByName('OwnerID');
+        $this->assertContains('Wolfgang', $field->FieldHolder(), 'Parent DataObject found.');
+    }
 
-	public function testSuggestions() {
+    public function testSuggestions()
+    {
+        $field = new DataObjectPicker('OwnerID', "Owner");
+        $field->setConfig('classToPick', 'DogOwner');
+        $suggestions = $field->Suggest(new SS_HTTPRequest('GET', 'someurl', array('request' => 'Ma')));
+        $this->assertContains('Martin', $suggestions, 'Suggest Martin.');
+        $this->assertContains('Mateusz', $suggestions, 'Suggest Mateusz.');
+        $this->assertNotContains('Wolfgang', $suggestions, 'Do not suggest Wolfgang.');
+    }
 
-		$field = new DataObjectPicker('OwnerID', "Owner");
-		$field->setConfig('classToPick', 'DogOwner');
-		$suggestions = $field->Suggest(new SS_HTTPRequest('GET', 'someurl', array('request' => 'Ma')));
-		$this->assertContains('Martin', $suggestions, 'Suggest Martin.');
-		$this->assertContains('Mateusz', $suggestions, 'Suggest Mateusz.');
-		$this->assertNotContains('Wolfgang', $suggestions, 'Do not suggest Wolfgang.');
-	}
+    public function testMakeReadonly()
+    {
+        $field = new DataObjectPicker('OwnerID', "Owner");
+        $field->setConfig('classToPick', 'DogOwner');
+        $field = $field->performReadonlyTransformation();
 
-	public function testMakeReadonly() {
-
-		$field = new DataObjectPicker('OwnerID', "Owner");
-		$field->setConfig('classToPick', 'DogOwner');
-		$field = $field->performReadonlyTransformation();
-
-		$this->assertContains('readonly', $field->Field(), 'Perform readonly transformation.');
-	}
-
+        $this->assertContains('readonly', $field->Field(), 'Perform readonly transformation.');
+    }
 }
 
-class DogOwner extends DataObject implements TestOnly {
+class DogOwner extends DataObject implements TestOnly
+{
 
-	static $db = array(
-		'Name' => 'Varchar',
-	);
+    public static $db = array(
+        'Name' => 'Varchar',
+    );
 
-	static $has_many = array(
-		'Dogs' => 'Dog',
-	);
+    public static $has_many = array(
+        'Dogs' => 'Dog',
+    );
 
-	static $summary_fields = array(
-		'Name' => 'Name',
-	);
+    public static $summary_fields = array(
+        'Name' => 'Name',
+    );
 }
 
-class Dog extends DataObject implements TestOnly {
+class Dog extends DataObject implements TestOnly
+{
 
-	static $db = array(
-		'Name' => 'Varchar',
-	);
+    public static $db = array(
+        'Name' => 'Varchar',
+    );
 
-	static $has_one = array(
-		'Owner' => 'DogOwner',
-	);
-	
-	static $summary_fields = array(
-		'Name',
-	);
+    public static $has_one = array(
+        'Owner' => 'DogOwner',
+    );
+    
+    public static $summary_fields = array(
+        'Name',
+    );
 
-	public function getCMSFields() {
-		return new FieldList(new DataObjectPicker('OwnerID', "Owner"));
-	}
+    public function getCMSFields()
+    {
+        return new FieldList(new DataObjectPicker('OwnerID', "Owner"));
+    }
 }
 
-class DogController extends Controller implements TestOnly {
+class DogController extends Controller implements TestOnly
+{
 
-	function getEditForm($id = null) {
-		return new Form($this, 'getEditForm', new FieldList(new DataObjectPicker('OwnerID', "Owner")), new FieldList());
-	}
+    public function getEditForm($id = null)
+    {
+        return new Form($this, 'getEditForm', new FieldList(new DataObjectPicker('OwnerID', "Owner")), new FieldList());
+    }
 
-	function Link() {
-		return 'DogController';
-	}
+    public function Link()
+    {
+        return 'DogController';
+    }
 }
